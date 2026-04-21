c2ef05c66d340ebbd611c07c3a2437091f15e01c
unknown <phero919@gmail.com>
2026-04-21 03:11:00 +0000

    updates to fix screen zoom on small devices

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,11 @@ export const metadata: Metadata = {
   description: "Generate Official Ghanaian Letters",
 };
 
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
